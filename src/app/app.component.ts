1763ef18a7c439faf4a85533a283d9d3709d61ec
Giano <bfcnorbi11@gmail.com>
2024-12-04 01:28:27 +0100

    Weboldal 1. prototípus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FirstMilestoneComponent } from './components/first-milestone/first-milestone.component';
-import { SecondMilestoneComponent } from './components/second-milestone/second-milestone.component';
-import { ThirdMilestoneComponent } from './components/third-milestone/third-milestone.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    FirstMilestoneComponent,
-    SecondMilestoneComponent,
-    ThirdMilestoneComponent
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Virtual Mouse';
-
-  // Változó a megjelenített komponenshez
-  currentComponent = 'first';
-
-  // Tab változtatás kezelése
-  onTabChange(index: number): void {
-    switch (index) {
-      case 0:
-        this.currentComponent = 'first';
-        break;
-      case 1:
-        this.currentComponent = 'second';
-        break;
-      case 2:
-        this.currentComponent = 'third';
-        break;
-    }
-  }
+  title = 'VirtualMouse';
 }
